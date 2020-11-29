75e70b8deb1fd577c0d9a5e02a0be53f10c9f5eb
Greg Rickaby <greg@gregrickaby.com>
2020-11-29 09:28:49 -0600

    add tailwindcss 2.0

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        margin: 'auto'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+  future: {}
+}
