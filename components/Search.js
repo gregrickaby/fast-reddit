102f1d481dd12b95c43cf7c957593a7500b21a5b
Greg Rickaby <gregrickaby@gmail.com>
2020-12-24 10:30:53 -0600

    fix the search bar on small screens

--- components/Search.js
+++ components/Search.js
@@ -13,22 +13,23 @@ export default function Search() {
   return (
     <>
       <form className="my-8 text-lg m-auto text-center" onSubmit={handleSubmit}>
-        <span className="mr-1">r/</span>
         <label htmlFor="search" className="sr-only">
           type the name of a subreddit to display posts
         </label>
-        <input
-          id="search"
-          type="text"
-          className="border p-2 w-1/2"
-          value={inputValue}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <input
-          className="border py-2 px-4 ml-1"
-          type="submit"
-          value="View Posts"
-        />
+        <div className="flex flex-1">
+          <input
+            id="search"
+            type="text"
+            className="border p-2 w-full"
+            value={inputValue}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <input
+            className="border py-2 px-4 ml-1"
+            type="submit"
+            value="View Posts"
+          />
+        </div>
       </form>
       <Results subreddit={subreddit} />
     </>
