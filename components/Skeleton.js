a34b039b5fffa8f3b75eb3828ea8aa2b7dab4d44
Greg Rickaby <gregrickaby@gmail.com>
2020-12-24 12:19:10 -0600

    add one more skeleton row

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -11,6 +11,7 @@ export default function Skeleton() {
       <div className="animate h-8 mt-8 rounded-3xl w-1/5"></div>
       <div className="animate h-8 mt-8 rounded-3xl w-3/4"></div>
       <div className="animate h-8 mt-8 rounded-3xl w-2/3"></div>
+      <div className="animate h-8 mt-8 rounded-3xl w-1/3"></div>
     </div>
   )
 }
