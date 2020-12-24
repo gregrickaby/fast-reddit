d990ea475febaf8c180bfc5f9e2bb3a52803f48f
Greg Rickaby <gregrickaby@gmail.com>
2020-12-24 14:24:57 -0600

    add <DisplayOptions />

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import config from '@/functions/config'
 import PropTypes from 'prop-types'
+import DisplayOptions from './DisplayOptions'
 import Footer from './Footer'
 import Header from './Header'
 import Meta from './Meta'
@@ -11,6 +12,7 @@ export default function Layout({children}) {
       <Header title={config.siteTitle} description={config.siteDescription} />
       <main className="container p-8 max-w-prose">{children}</main>
       <Footer author={config.siteAuthor} url={config.authorUrl} />
+      <DisplayOptions />
     </>
   )
 }
