5d7dad320de12fd695debd62285948fd8bbc1461
Greg Rickaby <gregrickaby@gmail.com>
2020-12-24 14:24:45 -0600

    build a font switcher

---
+++ components/DisplayOptions.js
@@ -0,0 +1,51 @@
+import clearFonts from '@/functions/clearFonts'
+import cn from 'classnames'
+import {useState} from 'react'
+
+export default function DisplayOptions() {
+  const [fontFamily, setFontFamily] = useState('font-sans')
+  const [fontSelector, toggleFontSelector] = useState(false)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    toggleFontSelector((prev) => !prev)
+  }
+
+  function fontSelect(event) {
+    event.preventDefault()
+    clearFonts()
+    setFontFamily(event.target.value)
+    document.body.classList.add(event.target.value)
+    toggleFontSelector(false)
+  }
+
+  return (
+    <div
+      className={cn('flex items-center fixed top-0 right-0 p-2', {
+        'bg-gray-900': fontSelector
+      })}
+    >
+      <form id="display" onSubmit={handleSubmit}>
+        <label htmlFor="display" className="sr-only">
+          select display options
+        </label>
+        <button className="px-2">{fontSelector ? 'X' : 'Aa'}</button>
+      </form>
+
+      {fontSelector && (
+        <div className="flex ml-2">
+          <select
+            id="fontSelect"
+            className="p-2 dark:text-gray-900"
+            value={fontFamily}
+            onChange={fontSelect}
+          >
+            <option value="font-sans">Sans Serif</option>
+            <option value="font-serif">Serif</option>
+            <option value="font-mono">Monospaced</option>
+          </select>
+        </div>
+      )}
+    </div>
+  )
+}
