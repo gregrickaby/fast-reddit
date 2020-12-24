9d6356b76101b1fe861352222c22da96e95451a9
Greg Rickaby <gregrickaby@gmail.com>
2020-12-24 10:51:16 -0600

    add authorUrl prop

--- functions/config.js
+++ functions/config.js
@@ -2,5 +2,6 @@ export default {
   siteTitle: 'Fast Reddit',
   siteDescription: 'Display a subreddit, fast!',
   siteUrl: 'https://fast-reddit.vercel.app/',
-  siteAuthor: '@gregrickaby'
+  siteAuthor: '@gregrickaby',
+  authorUrl: 'https://github.com/gregrickaby'
 }
