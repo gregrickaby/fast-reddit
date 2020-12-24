851fd67540bf73a2c3ea711c0a87750e2bb71e9f
Greg Rickaby <gregrickaby@gmail.com>
2020-12-24 10:51:02 -0600

    use header/footer in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
 import config from '@/functions/config'
 import PropTypes from 'prop-types'
+import Footer from './Footer'
+import Header from './Header'
 import Meta from './Meta'
 
 export default function Layout({children}) {
   return (
     <>
       <Meta />
-      <h1 className="text-4xl font-bold text-center">{config.siteTitle}</h1>
-      <p className="text-center">{config.siteDescription}</p>
+      <Header title={config.siteTitle} description={config.siteDescription} />
       <main className="container p-8 max-w-prose">{children}</main>
+      <Footer author={config.siteAuthor} url={config.authorUrl} />
     </>
   )
 }
