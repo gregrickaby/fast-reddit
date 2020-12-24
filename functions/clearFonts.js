a6cbc54b7f165051783c2ce9889cedbb47a77f8b
Greg Rickaby <gregrickaby@gmail.com>
2020-12-24 14:24:13 -0600

    create clearFonts function

---
+++ functions/clearFonts.js
@@ -0,0 +1,5 @@
+export default function clearFonts() {
+  document.body.classList.remove('font-sans')
+  document.body.classList.remove('font-mono')
+  document.body.classList.remove('font-serif')
+}
