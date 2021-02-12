89e6e1a9766f7e424a865d696a6ddaff0c540aca
Greg Rickaby <gregrickaby@gmail.com>
2021-02-12 15:45:38 -0600

    use loading css

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -1,18 +1,18 @@
 export default function Skeleton() {
   return (
     <div className="skeleton mt-10 m-auto text-center">
-      <div className="animate h-4 mt-4 rounded-3xl w-full"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-1/2"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-1/3"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-5/6"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-3/5"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-2/3"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-full"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-2/5"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-3/4"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-2/3"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-3/5"></div>
-      <div className="animate h-4 mt-4 rounded-3xl w-full"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-full"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-1/2"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-1/3"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-5/6"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-3/5"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-2/3"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-full"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-2/5"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-3/4"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-2/3"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-3/5"></div>
+      <div className="loading h-10 mt-4 rounded-3xl w-full"></div>
       <p className="mt-4">Loading posts...</p>
     </div>
   )
